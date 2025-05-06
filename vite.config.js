b9d4235310c0c4e7d75466f37a455e5cfe3e806f
Navpreet <navangural175@gmail.com>
2025-05-06 16:52:23 +0530

    image url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss()
   ],
+  build: {
+    outDir: 'build', // 👈 This changes the output folder from 'dist' to 'build'
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          antd: ['antd'],
+          axios: ['axios']
+        }
+
+      }
+    }
+  }
 })
