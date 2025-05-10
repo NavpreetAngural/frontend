16ed38cd8a489989381447b6858ab302d3c944bc
Navpreet <navangural175@gmail.com>
2025-05-11 00:45:57 +0530

    untraked file

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
+  plugins: [react(),
+    tailwindcss(),
   ],
-  build: {
-    outDir: 'dist', // ✅ Changed from 'build' to 'dist' for Netlify compatibility
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          antd: ['antd'],
-          axios: ['axios']
-        }
-      }
-    }
-  }
 })
